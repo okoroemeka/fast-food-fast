872a82ef85de055a179599b8d6ed40504373624d
okoroemeka <solomonokoro056@gmail.com>
2018-09-03 21:17:08 +0100

    feat(controller):get specific order
    - add controller for get specific order
    
    [Deliver #160232114]

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,14 +1,14 @@
-import http from 'http';
-import app from '../app';
+// import http from 'http';
+// import app from '../app';
 
-//  port for listening to request
-const port = parseInt(process.env.PORT, 10) || 8080;
-app.set('port', port);
+// //  port for listening to request
+// const port = parseInt(process.env.PORT, 10) || 8080;
+// app.set('port', port);
 
-// creating server
-const server = http.createServer(app);
+// // creating server
+// const server = http.createServer(app);
 
-// server listening at port
-server.listen(port, () => console.log(`listening on ${port}`));
+// // server listening at port
+// server.listen(port, () => console.log(`listening on port ${port}`));
 
-export default server;
+// export default server;
--- server/controller/order.js
+++ server/controller/order.js
@@ -1,16 +1,42 @@
 import seed from '../seed/dummydb';
-class Order{
-    static getAllOrder(req, res){
-        if (seed.length > 0) {
-            return res.status(200).json({
-                status: 'success',
-                data: seed,
-            })
-        }
-        return res.status(404).json({
-            status: 'fail',
-            message: 'No orders available'
-        })
+
+class Order {
+  /**
+   *@return {array} getAllOrder
+   * @param {*} req
+   * @param {*} res
+   */
+  static getAllOrder(req, res) {
+    if (seed.length > 0) {
+      return res.status(200).json({
+        status: 'success',
+        data: seed,
+      });
     }
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No orders available',
+    });
+  }
+
+  /**
+ * @return {object} getSpecificOrder
+ * @param {*} req
+ * @param {*} res
+ */
+  static getSpecificOrder(req, res) {
+    const { orderId } = req.params;
+    const order = seed.find(iterator => (parseInt(iterator.order_id, 10) === parseInt(orderId, 10)));
+    if (order) {
+      return res.status(200).json({
+        status: 'success',
+        data: order,
+      });
+    }
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No order found',
+    });
+  }
 }
 export default Order;

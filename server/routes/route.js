a333e6cf73ef59aa9bd8a7112b744be167068187
okoroemeka <solomonokoro056@gmail.com>
2018-09-04 13:44:48 +0100

    ft(API):create order
    - controller for create order
    
    [Deliver #160231723]

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,8 @@ import order from '../controller/order';
 // Express subrouter
 const router = express.Router();
 
+// Routing
 router.get('/order', order.getAllOrder);
 router.get('/order/:orderId', order.getSpecificOrder);
+router.post('/order', order.createOrder);
 export default router;

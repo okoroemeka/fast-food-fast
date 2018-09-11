5c842ffc805acf570b9aca006384df7c53fa79e6
okoroemeka <solomonokoro056@gmail.com>
2018-09-11 14:42:38 +0100

    add menu form

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,4 @@
+/* navigation bar*/
 let mainNav=document.getElementById('main-nav');
 let navbarToggle=document.getElementById('navbar-toggle');
 
@@ -12,4 +13,30 @@ navbarToggle.addEventListener('click',function(){
         this.classList.add('active');
     }
 });
+/* pop up*/
+let modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+let btn = document.getElementById("add-item-menu");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 console.log("connected");

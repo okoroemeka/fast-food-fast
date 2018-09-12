f6ded2d58f1d278e3c075ef16f0bc19e0dad0842
okoroemeka <solomonokoro056@gmail.com>
2018-09-12 14:17:40 +0100

    feat(controller): add controller
    - add controller for updating a menu item
    
    [Delivers #160446215]

--- server/controller/menu.js
+++ server/controller/menu.js
@@ -31,5 +31,41 @@ class Menu {
       message: 'All field are required',
     });
   }
+
+  // updat menu item
+  /**
+   * @returns {object} updateMenuItem
+   * @param {*} req 
+   * @param {*} res 
+   */
+  static upadeMenuItem(req, res){
+    const { itemId } = req.params;
+    const { food, price, itemImage } = req.body;
+
+    // checking if all feilds are not empty
+    if ((food !== undefined && food.trim().length !== 0) 
+    && (price !== undefined && price.trim().length !==0)
+    && (itemImage !== undefined && itemImage.trim().length)) {
+
+      // checking if item exits
+      const menuItem = menuDummyDb.find(item => parseInt(item.menuItem_id, 10) === parseInt(itemId, 10));
+      if (menuItem) {
+        menuDummyDb[itemId - 1] = Object.assign(menuItem, req.body);
+        return res.status(200).json({
+          status: 'success',
+          message: 'menu item updated successfully',
+          data: menuDummyDb[itemId - 1],
+        })
+      }
+      return res.status(404).json({
+        status: 'fail',
+        message: 'unable to update, item not found',
+      });
+    }
+    return res.status(400).json({
+      status: 'fail',
+      message: 'All field are required',
+    });
+  }
 }
 export default Menu;

f5c883a02cf006d032df043336beb7b365889a75
okoroemeka <solomonokoro056@gmail.com>
2018-09-19 15:39:33 +0100

    refactor code

--- server/seed/dummydb.js
+++ server/seed/dummydb.js
@@ -1,11 +1,11 @@
-// const order = [
-//   {
-//     name: 'okoro emeka',
-//     address: '2 cubic lane, Ikeja',
-//     order_id: 1,
-//     food: 'fried rice',
-//     price: '#1500',
-//     status: 'pending',
-//   },
-// ];
-// export default order;
+const order = [
+  {
+    name: 'okoro emeka',
+    address: '2 cubic lane, Ikeja',
+    order_id: 1,
+    food: 'fried rice',
+    price: '#1500',
+    status: 'pending',
+  },
+];
+export default order;

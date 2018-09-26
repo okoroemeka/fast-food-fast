d5e962242f5d02a6bdde63b966d2d46c754d52e4
okoroemeka <solomonokoro056@gmail.com>
2018-09-26 15:42:46 +0100

    ch(menu page): view total amount on modal form
    - display total when user enter quantity of item needed
    
    [Delivers #160793643]

--- UI/js/menu.js
+++ UI/js/menu.js
@@ -6,12 +6,31 @@ const orderButtons = document.querySelectorAll('.order-now')
 // let menubtn = document.getElementById("order-now");
 
 // Get the <span> element that closes the modal
-// const menuSpan = document.getElementsByClassName("close")[0];
 const menuSpan = document.getElementById("close");
 
 // When the user clicks the button, open the modal 
-orderButtons.forEach((button) => button.addEventListener('click',function () {
+orderButtons.forEach((button,i,orderButtons) => button.addEventListener('click',function () {
+    // get item price
+    let itemPrice = document.getElementsByClassName("Price")[i].innerText;
+    itemPrice = itemPrice.split(" ");
+    itemPrice = parseInt(itemPrice[itemPrice.length-1],10);
+        
+    // display modal containing form
     menuModal.style.display = "block";
+
+    let inputElement = document.getElementById('quantity');
+
+    // clear input area
+    inputElement.value = ``;
+
+    // clear item total container
+    document.getElementById('total-amount').innerText = ``;
+
+    // tracking changes in quantity input and adding event listener.
+    inputElement.oninput = function() {
+        let quantity = document.getElementById('quantity').value;
+        document.getElementById('total-amount').innerHTML = `Total: &#8358;${itemPrice * quantity}`;
+    };  
 }))
 
 // When the user clicks on <span> (x), close the modal

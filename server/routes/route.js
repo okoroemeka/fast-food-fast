c1f781d3edce38b84df1ac198e375258cde0125b
okoroemeka <solomonokoro056@gmail.com>
2018-09-29 15:36:30 +0100

    ft(signup):add user signup
    - add user signup
    
    - add validation for user signup
    
    [Delivers #160845294]

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import order from '../controller/order';
-
+import userController from '../controller/dbController/users';
+import signupValidation from '../utils/validation';
 // Express subrouter
 const router = express.Router();
 
 // Routing
-router.get('/orders', order.getAllOrder);
-router.get('/orders/:orderId', order.getSpecificOrder);
-router.post('/orders', order.createOrder);
-router.put('/orders/:orderId', order.updateOrderStatus);
+router.post('/auth/signup', signupValidation.signupValidation, userController.signUp);
+// router.post('/auth/signin', userController.signIn);
+// router.post('/auth/signin', users.signIn);
+// router.get('/orders', order.getAllOrder);
+// router.get('/orders/:orderId', order.getSpecificOrder);
+// router.post('/orders', order.createOrder);
+// router.put('/orders/:orderId', order.updateOrderStatus);
 // router.get('/', order.getHomePage);
 // router.all('*', order.catchEveryOtherRoute);
 
---
+++ server/utils/validation.js
@@ -0,0 +1,42 @@
+/* got this from
+https://www.codementor.io
+/olawalealadeusi896/building-a-simple-api-with-nodejs-expressjs-postgresql-db-and-jwt-3-mke10c5c5
+?utm_content=posts&utm_source=
+sendgrid&utm_medium=email&utm_term=
+post-mke10c5c5&utm_campaign=newsletter20180905 */
+import passwordValidation from './passwordValidatio';
+
+const validation = {
+  signupValidation: (req, res, next) => {
+    const {
+      fullname, email, password, confirmPassword,
+    } = req.body;
+    if (email === undefined || email.trim().length < 1 || !(/\S+@\S+\.\S+/.test(email))) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'please an email is required, make sure it follows this format[example@whatever.com]',
+      });
+    }
+    if (fullname === undefined || fullname.trim().length < 1 || typeof fullname !== 'string') {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'full name is needed and must be a string',
+      });
+    }
+    if (password === undefined || password.trim().length < 6
+    || typeof password !== 'string' || passwordValidation(password) !== 100) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'password length must greater than 6 and should contain uppercase,lowercase, number and any of this character[$@#&!]',
+      });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'the passwords do not match',
+      });
+    }
+    return next();
+  },
+};
+export default validation;

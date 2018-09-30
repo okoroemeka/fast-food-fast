4e7842af14d0fd87479e45a1d6e9a0fe03785124
okoroemeka <solomonokoro056@gmail.com>
2018-09-30 18:24:04 +0100

    ft(order):get specific order
    - add get specific order controller
    
    - add user authentication
    
    - add route for get specific order
    
    [Delivers #160232114]

--- server/controller/dbController/order.js
+++ server/controller/dbController/order.js
@@ -76,7 +76,7 @@ class Order {
         }
         return res.status(404).json({
           status: 'fail',
-          mesage: 'No available order',
+          mesage: 'order',
         });
       })
       .catch(() => res.status(500).json({
@@ -84,5 +84,93 @@ class Order {
         message: 'Internal server error, please try again later',
       }));
   }
+
+  /**
+ * @return {object} getSpecificOrder
+ * @param {*} req
+ * @param {*} res
+ */
+  static getSpecificOrder(req, res) {
+    const { orderId } = req.params;
+    const getSpecificOrderQuery = {
+      text: 'SELECT * FROM orders WHERE id=$1',
+      values: [parseInt(orderId, 10)],
+    };
+    if (req.decoded.status !== 'admin') {
+      return res.status(403).json({
+        status: 'fail',
+        message: 'You are not autthorized to perform this action',
+      });
+    }
+    return dbConnection.query(getSpecificOrderQuery)
+      .then((order) => {
+        if (order.rowCount !== 0) {
+          return res.status(200).json({
+            status: 'success',
+            data: order.rows[0],
+          });
+        }
+        return res.status(404).json({
+          status: 'fail',
+          message: 'Order not found',
+        });
+      })
+      .catch(() => res.status(500).json({
+        status: 'error',
+        message: 'Internal server error, please try again later',
+      }));
+  }
+  // /**
+  //  * @returns {object} updateOrderStatus
+  //  * @param {*} req
+  //  * @param {*} res
+  //  */
+  // static updateOrderStatus(req, res) {
+  //   const { orderId } = req.params;
+  //   const { status } = req.body;
+  //   const getSpecificOrderQuery = {
+  //     text: 'SELECT * FROM orders WHERE id=$1',
+  //     values: [parseInt(orderId, 10)],
+  //   };
+  //   const updateOrderQuery = {
+  //     text: 'UPDATE orders SET status=$1 WHERE id=$2 RETURNING *',
+  //     values: [status.trim(), parseInt(orderId, 10)],
+  //   };
+  //   if (status !== undefined && status.trim().length !== 0) {
+  //     if (status === 'accept' || status === 'decline') {
+  //       return dbConnection.query(getSpecificOrderQuery)
+  //         .then((order) => {
+  //           if (order.rowCount !== 0) {
+  //             return dbConnection.query(updateOrderQuery)
+  //               .then(upDatedOrder => res.status(200).json({
+  //                 status: 'success',
+  //                 message: 'Order status updated successfully',
+  //                 data: upDatedOrder.rows[0],
+  //               }))
+  //               .catch(() => res.status(500).json({
+  //                 status: 'error',
+  //                 message: 'Internal server error, please try again later',
+  //               }));
+  //           }
+  //           return res.status(404).json({
+  //             status: 'fail',
+  //             message: 'order does not exist',
+  //           });
+  //         })
+  //         .catch(() => res.status(500).json({
+  //           status: 'error',
+  //           message: 'Internal server error, please try again later',
+  //         }));
+  //     }
+  //     return res.status(400).json({
+  //       status: 'fail',
+  //       message: 'Status need to be either "accept" or "decline"',
+  //     });
+  //   }
+  //   return res.status(400).json({
+  //     status: 'fail',
+  //     message: 'Field can not be empty',
+  //   });
+  // }
 }
 export default Order;

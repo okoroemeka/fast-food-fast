f9f5ed16cd12e49b766279ad3bcc616b36e96558
okoroemeka <solomonokoro056@gmail.com>
2018-10-18 15:50:04 +0100

    ft(view): add view all orders
    - populate the view all order table
    
    - refactor get orders controller
    
    [Delivers #161314291]

---
+++ views/js/order.js
@@ -0,0 +1,44 @@
+const getOrders = () => {
+  const table = document.getElementById('online-order');
+  fetch('api/v1/orders', {
+    headers: {
+      'x-access-token': localStorage.getItem('token'),
+    },
+  })
+    .then(res => res.json())
+    .then((orders) => {
+      if (orders.status === 'success') {
+        let output = `<tr>
+                            <th>Name</th>
+                            <th>Order Id</th>
+                            <th>Item</th>
+                            <th>Quantity</th>
+                            <th>Response</th>
+                      </tr>`;
+        orders.orders.forEach((order) => {
+          output += `<tr>
+            <td>${order.fullname}</td>
+            <td>${order.id}</td>
+            <td>${order.food}</td>
+            <td>${order.quantity}</td>
+            <td>
+                <span>
+                    <button class="accept-button">Accept</button>
+                </span>
+                <span>
+                    <button class="reject-button">Decline</button>
+                </span>
+            </td>
+        </tr>`;
+        });
+        table.innerHTML = output;
+      } else if (orders.status === 'Fail') {
+        alert(orders.message);
+      } else {
+        alert(orders.message);
+      }
+    })
+    .catch(error => console.log(error));
+};
+window.addEventListener('load', getOrders);
+console.log('connected');

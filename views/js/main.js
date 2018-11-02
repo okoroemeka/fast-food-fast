b5027cb014fde67de761a16640aac353e4572121
okoroemeka <solomonokoro056@gmail.com>
2018-11-02 20:24:01 +0100

    ft(view): consume edit menu
    - consume edit menu api
    
    - refactor html and css code
    
    [Delivers #161550242]

--- views/js/main.js
+++ views/js/main.js
@@ -25,18 +25,20 @@ const btn = document.getElementById('add-item');
 
 const span = document.getElementById('close');
 // When the user clicks the button, open the modal
-btn.onclick = function () {
+btn.onclick = () => {
+  document.getElementById('form-header').innerText = 'Add Menu';
+  document.getElementById('action-button').innerText = 'Add Item';
   modal.style.display = 'block';
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.onclick = () => {
   modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+window.onclick = (event) => {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
 };
